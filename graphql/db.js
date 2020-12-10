64a70da59529e84991002f3980954b3c5a2bec0b
jeonghoheo <heojeongho1992@gmail.com>
2020-12-10 20:26:22 +0900

    add - Mutation 정의하기

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,48 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: "Jeongho",
-    age: 18,
-    gender: "male"
+    name: "Star Wars - The new one",
+    score: 1
   },
   {
     id: 1,
-    name: "choco",
-    age: 18,
-    gender: "female"
+    name: "Avengers - The new one",
+    score: 8
   },
   {
     id: 2,
-    name: "lalal",
-    age: 18,
-    gender: "male"
+    name: "The Godfather I",
+    score: 99
   },
   {
     id: 3,
-    name: "TEst1",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 4,
-    name: "TEst2",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 5,
-    name: "TEst3",
-    age: 18,
-    gender: "male"
-  },
-  {
-    id: 6,
-    name: "TEst4",
-    age: 18,
-    gender: "male"
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  return movies.filter((movie) => movie.id === id)[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movie.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
